18f42de4c91b00426ec4511fa57f7fca1fe1ca14
lrg <lrg47@yahoo.com>
2012-01-17 11:26:42 -0500

    Removed default Rails page - lrg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+   #Return a title on a per-page basis
+   #entered by lrg on 1/3/2011 from ROR tutorial Listing 4.2
+   def title
+      base_title="Ruby on Rails Tutorial Sample App"
+      if @title.nil?
+	 base_title
+      else
+	 "#{base_title} | #{@title}"
+      end
+   end
 end
